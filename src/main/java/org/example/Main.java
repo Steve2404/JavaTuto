8455a1e590d9559ca86fbc4ad452a9cac01bda8f
Steve2404 <81958298+Steve2404@users.noreply.github.com>
2025-10-02 06:04:43 +0200

    generate sql query and create datebase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,26 +3,32 @@ package org.example;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UTFDataFormatException;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
 
-    /*
+
     static String getMondialSql() {
+
+        // https://github.com/ullenboom/mondial-database
         try (InputStream stream = Main.class.getResourceAsStream("/db/mondial.sql")) {
-            //new String(stream.readAllBytes(), )
-        } catch (IOException e) {
+            return new String(stream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
-    */
+
 
 
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection("jdbc:h2:file:./mondial")) {
-            System.out.println(connection);
-            // https://github.com/ullenboom/mondial-database
+            // String sql = getMondialSql();
+            // connection.createStatement().execute(sql);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
